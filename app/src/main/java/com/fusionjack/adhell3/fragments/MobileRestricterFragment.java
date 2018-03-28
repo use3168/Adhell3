0dd6a8a7143db2f29f8f0b3b97e58d563f0e889e
fusionjack <dogfight60-fusionjack@yahoo.de>
2018-03-28 17:57:48 +0200

    Readd import/export for the time being
    Even the database is now on internal storage, the database might have changed due to ongoing development.
    If the database needs to be deleted, the import function is still needed.
    Readd import/export functionality for the time being until a proper backup/restore is implemented.

--- app/src/main/java/com/fusionjack/adhell3/fragments/MobileRestricterFragment.java
+++ app/src/main/java/com/fusionjack/adhell3/fragments/MobileRestricterFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -32,6 +33,12 @@ import com.fusionjack.adhell3.db.AppDatabase;
 import com.fusionjack.adhell3.db.entity.AppInfo;
 import com.fusionjack.adhell3.utils.AppsListDBInitializer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -153,15 +160,86 @@ public class MobileRestricterFragment extends Fragment {
                 Toast.makeText(context, getString(R.string.app_list_sorted_by_restricted), Toast.LENGTH_SHORT).show();
                 loadApplicationsList(false);
                 break;
-            case R.id.action_pack_dis_reload:
-                editText.setText("");
-                loadApplicationsList(true);
+            case R.id.restricter_import_storage:
+                Toast.makeText(context, getString(R.string.imported_restricted_from_storage), Toast.LENGTH_SHORT).show();
+                importList();
+                break;
+            case R.id.restricter_export_storage:
+                Toast.makeText(context, getString(R.string.exported_restricted_to_storage), Toast.LENGTH_SHORT).show();
+                exportList();
                 break;
             case R.id.restricter_enable_all:
                 Toast.makeText(context, getString(R.string.enabled_all_restricted), Toast.LENGTH_SHORT).show();
                 enableAllPackages();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void importList() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... o) {
+                File file = new File(Environment.getExternalStorageDirectory(), "mobile_restricted_packages.txt");
+
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        try {
+                            AppInfo appInfo = mDb.applicationInfoDao().getByPackageName(line);
+                            appInfo.mobileRestricted = true;
+                            mDb.applicationInfoDao().insert(appInfo);
+                        }
+                        catch (Exception e) {
+                            // Ignore any potential errors
+                        }
+                    }
+                }
+                catch (IOException e) {
+                    Log.e("Exception", "File write failed: " + e.toString());
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void o) {
+                super.onPostExecute(o);
+                loadApplicationsList(false);
+            }
+        }.execute();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void exportList() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... o) {
+                File file = new File(Environment.getExternalStorageDirectory(), "mobile_restricted_packages.txt");
+                List<AppInfo> restrictedAppList = mDb.applicationInfoDao().getMobileRestrictedApps();
+
+                try {
+                    FileOutputStream stream = new FileOutputStream(file);
+                    OutputStreamWriter writer = new OutputStreamWriter(stream);
+
+                    writer.write("");
+
+                    for (AppInfo app : restrictedAppList) {
+                        writer.append(app.packageName + "\n");
+                    }
+
+                    writer.close();
+                    stream.flush();
+                    stream.close();
+                }
+                catch (IOException e) {
+                    Log.e("Exception", "File write failed: " + e.toString());
+                }
+
+                return null;
+            }
+        }.execute();
     }
 
     @SuppressLint("StaticFieldLeak")
--- app/src/main/java/com/fusionjack/adhell3/fragments/PackageDisablerFragment.java
+++ app/src/main/java/com/fusionjack/adhell3/fragments/PackageDisablerFragment.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -35,6 +36,7 @@ import com.fusionjack.adhell3.db.entity.AppInfo;
 import com.fusionjack.adhell3.utils.AppsListDBInitializer;
 
 import java.util.List;
+import java.io.*;
 
 import javax.inject.Inject;
 
@@ -160,15 +162,87 @@ public class PackageDisablerFragment extends Fragment {
                 Toast.makeText(context, getString(R.string.app_list_sorted_by_disabled), Toast.LENGTH_SHORT).show();
                 loadApplicationsList(false);
                 break;
-            case R.id.action_pack_dis_reload:
-                editText.setText("");
-                loadApplicationsList(true);
+            case R.id.disabler_import_storage:
+                Toast.makeText(context, getString(R.string.imported_from_storage), Toast.LENGTH_SHORT).show();
+                importList();
+                break;
+            case R.id.disabler_export_storage:
+                Toast.makeText(context, getString(R.string.exported_to_storage), Toast.LENGTH_SHORT).show();
+                exportList();
                 break;
             case R.id.disabler_enable_all:
                 Toast.makeText(context, getString(R.string.enabled_all_disabled), Toast.LENGTH_SHORT).show();
                 enableAllPackages();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void importList() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... o) {
+                File file = new File(Environment.getExternalStorageDirectory(), "adhell_packages.txt");
+
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        try {
+                            AppInfo appInfo = mDb.applicationInfoDao().getByPackageName(line);
+                            appInfo.disabled = true;
+                            appPolicy.setDisableApplication(line);
+                            mDb.applicationInfoDao().insert(appInfo);
+                        }
+                        catch (Exception e) {
+                            // Ignore any potential errors
+                        }
+                    }
+                }
+                catch (IOException e) {
+                    Log.e("Exception", "File write failed: " + e.toString());
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void o) {
+                super.onPostExecute(o);
+                loadApplicationsList(true);
+            }
+        }.execute();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void exportList() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... o) {
+                File file = new File(Environment.getExternalStorageDirectory(), "adhell_packages.txt");
+                List<AppInfo> disabledAppList = mDb.applicationInfoDao().getDisabledApps();
+
+                try {
+                    FileOutputStream stream = new FileOutputStream(file);
+                    OutputStreamWriter writer = new OutputStreamWriter(stream);
+
+                    writer.write("");
+
+                    for (AppInfo app : disabledAppList) {
+                        writer.append(app.packageName + "\n");
+                    }
+
+                    writer.close();
+                    stream.flush();
+                    stream.close();
+                }
+                catch (IOException e) {
+                    Log.e("Exception", "File write failed: " + e.toString());
+                }
+
+                return null;
+            }
+        }.execute();
     }
 
     @SuppressLint("StaticFieldLeak")
