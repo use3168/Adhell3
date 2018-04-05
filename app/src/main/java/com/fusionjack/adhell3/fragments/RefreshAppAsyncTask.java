79b536e993f7ba3931a29d08a52adc6bbc6afbb7
fusionjack <dogfight60-fusionjack@yahoo.de>
2018-04-06 00:56:16 +0200

    Fix wrong logic on refreshing apps

--- app/src/main/java/com/fusionjack/adhell3/fragments/RefreshAppAsyncTask.java
+++ app/src/main/java/com/fusionjack/adhell3/fragments/RefreshAppAsyncTask.java
@@ -10,6 +10,7 @@ import com.fusionjack.adhell3.R;
 import com.fusionjack.adhell3.db.AppDatabase;
 import com.fusionjack.adhell3.db.entity.AppInfo;
 import com.fusionjack.adhell3.db.entity.DisabledPackage;
+import com.fusionjack.adhell3.db.entity.FirewallWhitelistedPackage;
 import com.fusionjack.adhell3.db.entity.RestrictedPackage;
 import com.fusionjack.adhell3.utils.AdhellAppIntegrity;
 import com.fusionjack.adhell3.utils.AppsListDBInitializer;
@@ -33,59 +34,69 @@ public class RefreshAppAsyncTask extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected Void doInBackground(Void... voids) {
-        // Get first disabled and restricted apps before they get deleted
+        // Get first disabled, restricted and whitelisted apps before they get deleted
         AppDatabase appDatabase = appFlag.getAppDatabase();
         List<AppInfo> disabledApps = appDatabase.applicationInfoDao().getDisabledApps();
         List<AppInfo> restrictedApps = appDatabase.applicationInfoDao().getMobileRestrictedApps();
+        List<FirewallWhitelistedPackage> whitelistedApps = appDatabase.firewallWhitelistedPackageDao().getAll();
 
         // Delete all apps info
         appDatabase.applicationInfoDao().deleteAll();
         AppsListDBInitializer.getInstance().fillPackageDb(appFlag.getPackageManager());
 
-        switch (appFlag.getFlag()) {
-            case DISABLER_FLAG:
-                ApplicationPolicy appPolicy = appFlag.getAppPolicy();
-                if (appPolicy == null) {
-                    break;
-                }
+        // Disable apps
+        ApplicationPolicy appPolicy = appFlag.getAppPolicy();
+        appDatabase.disabledPackageDao().deleteAll();
+        List<DisabledPackage> disabledPackages = new ArrayList<>();
+        for (AppInfo oldAppInfo : disabledApps) {
+            appPolicy.setEnableApplication(oldAppInfo.packageName);
+            AppInfo newAppInfo = appDatabase.applicationInfoDao().getByPackageName(oldAppInfo.packageName);
+            if (newAppInfo != null) {
+                newAppInfo.disabled = true;
+                appDatabase.applicationInfoDao().insert(newAppInfo);
+                appPolicy.setDisableApplication(newAppInfo.packageName);
 
-                appDatabase.disabledPackageDao().deleteAll();
-                List<DisabledPackage> disabledPackages = new ArrayList<>();
-                for (AppInfo oldAppInfo : disabledApps) {
-                    appPolicy.setEnableApplication(oldAppInfo.packageName);
-                    AppInfo newAppInfo = appDatabase.applicationInfoDao().getByPackageName(oldAppInfo.packageName);
-                    if (newAppInfo != null) {
-                        newAppInfo.disabled = true;
-                        appDatabase.applicationInfoDao().insert(newAppInfo);
-                        appPolicy.setDisableApplication(newAppInfo.packageName);
-
-                        DisabledPackage disabledPackage = new DisabledPackage();
-                        disabledPackage.packageName = newAppInfo.packageName;
-                        disabledPackage.policyPackageId = AdhellAppIntegrity.DEFAULT_POLICY_ID;
-                        disabledPackages.add(disabledPackage);
-                    }
-                }
-                appDatabase.disabledPackageDao().insertAll(disabledPackages);
-                break;
-
-            case RESTRICTED_FLAG:
-                appDatabase.restrictedPackageDao().deleteAll();
-                List<RestrictedPackage> restrictedPackages = new ArrayList<>();
-                for (AppInfo oldAppInfo : restrictedApps) {
-                    AppInfo newAppInfo = appDatabase.applicationInfoDao().getByPackageName(oldAppInfo.packageName);
-                    if (newAppInfo != null) {
-                        newAppInfo.mobileRestricted = true;
-                        appDatabase.applicationInfoDao().insert(newAppInfo);
-
-                        RestrictedPackage restrictedPackage = new RestrictedPackage();
-                        restrictedPackage.packageName = newAppInfo.packageName;
-                        restrictedPackage.policyPackageId = AdhellAppIntegrity.DEFAULT_POLICY_ID;
-                        restrictedPackages.add(restrictedPackage);
-                    }
-                }
-                appDatabase.restrictedPackageDao().insertAll(restrictedPackages);
-                break;
+                DisabledPackage disabledPackage = new DisabledPackage();
+                disabledPackage.packageName = newAppInfo.packageName;
+                disabledPackage.policyPackageId = AdhellAppIntegrity.DEFAULT_POLICY_ID;
+                disabledPackages.add(disabledPackage);
+            }
         }
+        appDatabase.disabledPackageDao().insertAll(disabledPackages);
+
+        // Restricted apps
+        appDatabase.restrictedPackageDao().deleteAll();
+        List<RestrictedPackage> restrictedPackages = new ArrayList<>();
+        for (AppInfo oldAppInfo : restrictedApps) {
+            AppInfo newAppInfo = appDatabase.applicationInfoDao().getByPackageName(oldAppInfo.packageName);
+            if (newAppInfo != null) {
+                newAppInfo.mobileRestricted = true;
+                appDatabase.applicationInfoDao().insert(newAppInfo);
+
+                RestrictedPackage restrictedPackage = new RestrictedPackage();
+                restrictedPackage.packageName = newAppInfo.packageName;
+                restrictedPackage.policyPackageId = AdhellAppIntegrity.DEFAULT_POLICY_ID;
+                restrictedPackages.add(restrictedPackage);
+            }
+        }
+        appDatabase.restrictedPackageDao().insertAll(restrictedPackages);
+
+        // Whitelisted apps
+        appDatabase.firewallWhitelistedPackageDao().deleteAll();
+        List<FirewallWhitelistedPackage> whitelistedPackages = new ArrayList<>();
+        for (FirewallWhitelistedPackage oldAppInfo : whitelistedApps) {
+            AppInfo newAppInfo = appDatabase.applicationInfoDao().getByPackageName(oldAppInfo.packageName);
+            if (newAppInfo != null) {
+                newAppInfo.adhellWhitelisted = true;
+                appDatabase.applicationInfoDao().insert(newAppInfo);
+
+                FirewallWhitelistedPackage whitelistedPackage = new FirewallWhitelistedPackage();
+                whitelistedPackage.packageName = newAppInfo.packageName;
+                whitelistedPackage.policyPackageId = AdhellAppIntegrity.DEFAULT_POLICY_ID;
+                whitelistedPackages.add(whitelistedPackage);
+            }
+        }
+        appDatabase.firewallWhitelistedPackageDao().insertAll(whitelistedPackages);
 
         return null;
     }
